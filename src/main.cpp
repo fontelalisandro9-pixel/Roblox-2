becdd3d0636f624639064a4e95fb4cbc2ea20cdb
fontelalisandro9-pixel <fontelalisandro9@gmail.com>
2026-02-24 15:31:26 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,18 @@
 using namespace geode::prelude;
 
 class $modify(MyMenuLayer, MenuLayer) {
-	bool init() {
-		if (!MenuLayer::init()) return false;
+    bool init() {
+        if (!MenuLayer::init()) return false;
 
-		auto label = CCLabelBMFont::create("Roblox 2 Mod Loaded!", "bigFont.fnt");
-		label->setPosition({CCDirector::get()->getWinSize().width / 2, 50});
-		label->setScale(0.5f);
-		this->addChild(label);
+        auto label = CCLabelBMFont::create("Roblox 2 Mod Loaded!", "bigFont.fnt");
+        label->setPosition({CCDirector::get()->getWinSize().width / 2, 50});
+        label->setScale(0.5f);
+        this->addChild(label);
 
-		log::info("Hello from Roblox 2 Mod, lisi445!");
+        log::info("Hello from Roblox 2 Mod, lisi445!");
 
-		return true;
-	}
+        return true;
+    }
 };
+
 
